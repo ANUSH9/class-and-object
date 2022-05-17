c79f3e209958b4a55b020fea00cde1e02c860435
Anush Gupta <loginanushgupta@gmail.com>
2022-05-17 15:56:36 +0530

    class

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mobile
+{
+        public class cellphone
+        {
+            string name;
+            string city;
+            int modnumber;
+            int phonenumber;
+            public void phone()
+            {
+                name = "samsung";
+                city = "Delhi";
+                modnumber = 5;
+                phonenumber = 8076485;
+                Console.WriteLine(name);
+                Console.WriteLine(city);
+                Console.WriteLine(modnumber);
+                Console.WriteLine(phonenumber);
+
+            }
+        }
+    
+}
