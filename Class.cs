43407e270169f81f291de101f25099212c26a0e8
Anush Gupta <loginanushgupta@gmail.com>
2022-05-17 15:57:30 +0530

    object

---
+++ Class.cs
@@ -0,0 +1,19 @@
+﻿using mobile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mobile
+{
+    class Class
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello anush gupta");
+            cellphone obj = new cellphone();
+            obj.phone();
+        }
+    }
+}
