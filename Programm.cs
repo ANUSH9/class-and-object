10fa7d4138b4120228562782c9d94d3acc9da75b
Anush Gupta <loginanushgupta@gmail.com>
2022-05-20 17:15:47 +0530

    file handling

---
+++ Programm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace work
+
+{
+    public class network
+    {
+        public void ReadLines()
+        {
+            FileStream fileStreamobj = new FileStream("F:\\132\\network.txt", FileMode.Open, FileAccess.Read);
+            StreamReader streamReaderobj = new StreamReader(fileStreamobj);
+            Console.WriteLine("Id\tSource\tDestination\tDate\tTime\tStatus\tNetwork");
+            
+            while (streamReaderobj.Peek() > 0)
+            {
+                string line = streamReaderobj.ReadLine();
+                
+                if (line != "")
+                {
+                    if (line.StartsWith("Date"))
+                    {
+                        string[] fine2 = line.Split(' ');
+
+                        string[] fine1 = fine2[0].Split(':');
+
+                        Console.Write(fine1[1] + "\t" + fine2[1] + "\t");
+                        continue;
+                    }
+                    string[] fine = line.Split(':');
+                    Console.Write(fine[1] + "\t");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+                
+                
+
+            }
+        }
+    }
+}
---
+++ networklog.cs
@@ -0,0 +1,18 @@
+﻿using work;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace networks
+
+{
+    public class Program
+{
+    static void Main(string[] args)
+    {
+        network readobj = new network();
+        readobj.ReadLines();
+    }
+}
+}
